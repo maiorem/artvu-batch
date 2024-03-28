b97867ffb2e0db041f7051be635483685799de1c
Seyoung Hong <maiorem00@gmail.com>
2024-03-28 23:56:11 +0900

    mi commit

---
+++ src/main/java/com/artvu/batch/ArtvubatchApplication.java
@@ -0,0 +1,13 @@
+package com.artvu.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtvubatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtvubatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artvu/batch/ArtvubatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artvu.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtvubatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

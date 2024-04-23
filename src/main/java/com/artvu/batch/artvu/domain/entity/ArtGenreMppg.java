aaa3c5cbddf6120b2b152923ca50c6b63c189d27
Seyoung Hong <sehong@nuskin.com>
2024-04-23 15:28:24 +0900

    feat: #1 - moving project

---
+++ src/main/java/com/artvu/batch/artvu/domain/entity/ArtGenreMppg.java
@@ -0,0 +1,33 @@
+package com.artvu.batch.artvu.domain.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Comment("상품장르연결내역")
+@Table(name = "TB_ART_GENRE_MPPG")
+public class ArtGenreMppg {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ART_GENRE_MPPG_ID")
+    @Comment("장르매핑ID")
+    private int artGenreMppgId;
+
+    @ManyToOne
+    @JoinColumn(name = "ART_ID")
+    @Comment("상품_상품ID")
+    private ArtList artList;
+
+    @ManyToOne
+    @JoinColumn(name = "ART_GENRE_ID")
+    @Comment("장르_장르번호")
+    private GenreList genreList;
+
+}
---
+++ src/main/java/com/artvu/batch/artvu/domain/entity/ArtList.java
@@ -0,0 +1,73 @@
+package com.artvu.batch.artvu.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Comment;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@Builder
+@Table(name = "TB_ART")
+@Comment("상품목록")
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArtList {
+
+    @Id
+    @Column(length = 20, name="ART_ID")
+    private String artId;
+
+    @Column(length = 200, name = "ART_NM")
+    @Comment("공연명")
+    private String artNm;
+
+    @Comment("공연시설ID")
+    @JoinColumn(name = "ART_FAC_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private ArtFacDetail artFacId;
+
+    @Column(name = "ORG_PRICE")
+    @Comment("원본가격")
+    @ColumnDefault("0")
+    private int orgPrice;
+
+    @Column(name = "MIN_PRICE")
+    @Comment("최저가격")
+    @ColumnDefault("0")
+    private int minPrice;
+
+    @Column(name = "MIN_PRICE_REG_DT")
+    @Comment("최저가격기준시간")
+    private LocalDateTime minPriceRegDt;
+
+    @Column(length = 10, name = "STATUS")
+    @Comment("공연상태")
+    private String status;
+
+    @Column(length = 2000, name = "COPY_TEXT")
+    @Comment("카피라이터")
+    private String copyText;
+
+    @Column(length = 50, name = "ART_SHOW_AGE")
+    @Comment("공연관람연령")
+    private String artShowAge;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "artList")
+    private List<ArtGenreMppg> artGenreMppgs = new ArrayList<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "artList")
+    private List<ArtImg> artImgList = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "AREA_CODE")
+    @Comment("지역정보-지역코드")
+    private ArtArea areaCode;
+
+}

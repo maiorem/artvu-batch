b3b0b3346f143bc3d2565d2c62c99a1d1a71853e
Seyoung Hong <maiorem00@gmail.com>
2024-04-26 23:14:57 +0900

    feat: #2 - 뮤지컬 목록에 추가

---
+++ src/main/java/com/artvu/batch/artlist/application/ArtMusicalListItemWriter.java
@@ -0,0 +1,66 @@
+package com.artvu.batch.artlist.application;
+
+import com.artvu.batch.artlist.domain.entity.KopisArtList;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+public class ArtMusicalListItemWriter<T> extends JpaItemWriter<List<KopisArtList>> {
+
+
+    private JpaItemWriter<KopisArtList> jpaItemWriter;
+
+    @Autowired
+    private ArtListService artService;
+
+    public ArtMusicalListItemWriter(JpaItemWriter<KopisArtList> jpaItemWriter) {
+        this.jpaItemWriter = jpaItemWriter;
+    }
+
+    @Override
+    @Transactional
+    public void write(Chunk<? extends List<KopisArtList>> items) {
+        log.info("art musical LIST WRITER ================================================");
+
+        LocalDate today = LocalDate.now(ZoneId.of("Asia/Seoul"));
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+
+        List<String> artIdList = artService.artIdList();
+
+        Chunk<KopisArtList> collect = new Chunk<>();
+        for(List<KopisArtList> list : items){
+            List<KopisArtList> newList = new ArrayList<>();
+            for (KopisArtList kopisArtList : list) {
+                if (!artIdList.contains(kopisArtList.getArtId())) {
+                    newList.add(kopisArtList);
+                } else {
+                    if (today.isAfter(LocalDate.parse(kopisArtList.getArtStrDt(), dateFormat) )) {
+                        kopisArtList.setStatus("공연중");
+                    }
+                    if ( today.isAfter(LocalDate.parse(kopisArtList.getArtEndDt(), dateFormat) ) ) {
+                        kopisArtList.setStatus("공연종료");
+                    }
+                }
+            }
+            collect.addAll(newList);
+        }
+        jpaItemWriter.write(collect);
+    }
+
+    @Override
+    public void afterPropertiesSet()  {
+        Assert.notNull(jpaItemWriter, "For art musical list, An entity manager is required");
+    }
+}

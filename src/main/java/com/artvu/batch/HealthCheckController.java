9776f9a85b19a255321f60c157e7f73d1945ce4d
Seyoung Hong <maiorem00@gmail.com>
2024-05-01 19:30:52 +0900

    feat: #3 - 헬스 체크

---
+++ src/main/java/com/artvu/batch/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.artvu.batch;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "artvu batch health check !";
+    }
+
+
+}
